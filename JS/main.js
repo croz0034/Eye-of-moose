7be0bddfba22c161daafcd270393546b25404f3a
croz0034 <32398078+croz0034@users.noreply.github.com>
2017-10-16 18:03:24 -0400

    bringing online

---
+++ JS/main.js
@@ -0,0 +1,47 @@
+
+////////////////// Table Clearing
+function clear(){
+     let ork = document.getElementById('ork')
+    ork.classList.toggle('hidden');
+    let clear = document.getElementById('people')
+    clear.innerHTML= ' ';
+    clear.classList.toggle('hidden');
+}
+///////////////// Data Retrieval
+function dumpRetrieve(){
+    let names= document.getElementById("dataDump");
+    let people= names.textContent;
+    let waivered= people.split(" ");
+    return waivered
+}
+////////////////////Input Gatherer
+function test() {
+    let x = document.getElementById('orc');
+    let text = x.value;
+    let people= text;
+    let waivered= people.split(" ");
+    let dataDump = document.getElementById("dataDump");
+    dataDump.textContent= text;
+    let attendance = document.getElementById('attendance'); 
+attendance.textContent= waivered.length;
+}
+/////////////////////View Waivered List command
+function exhaustive(){
+    clear();
+        let drama = dumpRetrieve();
+    for (i=0; i<drama.length; i++){
+        let people = document.getElementById('people')
+        let person = document.createElement('p')
+        person.textContent = drama[i];
+        people.appendChild(person);
+    }
+}
+////////////////////// Check Waiver Command
+function Waiver() {
+    let haswaiver = 0;
+    let person = prompt("Character name please");
+    let drama = dumpRetrieve();
+haswaiver = drama.indexOf(person);
+    haswaiver >= 0? 
+        alert(person + " has signed a waver! Have fun swinging foam!") : alert (person + " has NOT signed a waver! Get on that >:\(");    
+}
